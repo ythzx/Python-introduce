1858ca1452bba09a04035bdfb545869ce1b52128
thinkinghere <xinzhifufa@126.com>
2019-11-06 23:56:57 +0800

    add mro

--- 22 面向对象/class.py
+++ 22 面向对象/class.py
@@ -1,6 +1,8 @@
 # coding: utf8
 
 # 条目基类
+
+
 class Subject:
     kind = None
 
@@ -11,15 +13,16 @@ class Subject:
 
     def show_title(self):
         return self.title
-    
+
     def update_title(self, title):
         self.title = title
 
+
 # 类的实例化
-subject = Subject(1, 1000,'条目1')
+subject = Subject(1, 1000, '条目1')
 
 # 获取对象的属性
-print(subject.kind, subject.id, subject.category_id,subject.title)
+print(subject.kind, subject.id, subject.category_id, subject.title)
 
 # 调用对象的方法
 res = subject.show_title()
@@ -35,16 +38,56 @@ res2 = subject.title
 class Movie(Subject):
     kind = 'movie'
 
-    def __init__(self, id, category_id, title, directors = []):
+    def __init__(self, id, category_id, title, directors=[]):
         super.__init__(id, category_id, title)
         self.directors = directors
-    
+
     def show_directors(self):
         return self.directors
-    
+
     def show_title(self):
-        return f'Movie: {self.title}'  # 多态 
+        return f'Movie: {self.title}'  # 多态
 
 # MRO 方法解析顺序 Methord Resolution Order
 
+class A:
+    def run(self):
+        print("A.run")
+    
+class B(A):
+    pass
 
+class C(A):
+    def run(self):
+        print("C.run")
+
+class D(B,C):
+    pass
+
+# 菱形继承 
+#   A
+#  / \
+# /   \
+# B   C
+# \   /
+#  \ /
+#   D
+
+# 经典类继承顺序 DBAC
+
+import inspect
+
+inspect.getmro(D)
+# Out[3]:
+# (<class __main__.D at 0x00000000041DABE8>,
+#  <class __main__.B at 0x0000000004037B88>,
+#  <class __main__.A at 0x000000000414CB28>,
+#  <class __main__.C at 0x0000000004037A68>)
+
+
+# 新式类继承顺序 DBCA
+
+inspect.getmro(D)
+# Out[6]: (__main__.D, __main__.B, __main__.C, __main__.A, object)
+
+# 深度优先会查找到基类，基类中的方法通常是占位用的
