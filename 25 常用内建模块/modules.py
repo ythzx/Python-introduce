528e4b51cf1084354524a3ea8727c155ede3349e
thinkinghere <xinzhifufa@126.com>
2019-11-23 22:32:24 +0800

    add csv

--- 25 常用内建模块/modules.py
+++ 25 常用内建模块/modules.py
@@ -35,3 +35,52 @@ os.path.basename(p) # 获取指定路径文件名字
 os.path.dirname(p) # 获取指定文件路径名字
 
 os.path.exists(p) # 判断文件或者路径是否存在
+
+
+# sys
+import sys
+
+sys.getrefcount() # 引用计数
+sys.getsizeof() # 占用的字节数
+
+for obj in ({}, [], (), 'string', 1, 12.3):
+    print(obj.__class__.__name__, sys.getsizeof(d))
+
+# dict 64   
+# list 64   
+# tuple 64  
+# str 64    
+# int 64    
+# float 64  
+
+# sys.argvs
+
+import sys
+
+script_name, *args = sys.argv  # python3的解包
+print(f'Script: {script_name}') # 文件名
+print(f'Arguments: {args}') # 传入的参数
+
+# python argv.py -v
+
+
+# csv
+
+import csv 
+
+with open('test.csv', 'wt') as f:
+    writer = csv.writer(f)
+    writer.writerow(('id', '用户', '类型'))
+    for i in range(3):
+        row = (i, f'用户{i}', f'类型{i}')
+        writer.writerow(row)
+    
+with open('test.csv', 'rt') as f:
+    reader = csv.DictReader(f)
+    for i in reader:
+        print(i)
+
+# datetime
+
+import datetime
+
